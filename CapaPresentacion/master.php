8cfa94b3bc0392914e77b4f80fa346830a44ad1f
asansola <asansola@gmail.com>
2015-07-12 10:23:39 -0600

    Funcionalidad del Boton toggle (para mobiles)

--- CapaPresentacion/master.php
+++ CapaPresentacion/master.php
@@ -35,18 +35,20 @@
 	<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
 		<div class="container ">
 
-			<!-- Brand and toggle get grouped for better mobile display -->
+			<!-- Brand and toggle get grouped for better mobile display -->   <!-- Agrega el objetivo para agreagr el menu en dispositivos mobiles -->
 			<div class="navbar-header">
 				<button type="button" class="navbar-toggle collapsed"
-					data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
+					data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"   
 					aria-expanded="false">
-					<span class="sr-only">Toggle navigation</span> <span
-						class="icon-bar"></span> <span class="icon-bar"></span> <span
-						class="icon-bar"></span>
+					<span class="sr-only">Toggle navigation</span> 
+					<span class="icon-bar"></span> 
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
 				</button>
 				<a class="navbar-brand "  href="index.php" >Inicio</a>
 			</div>
-		
+			
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			<ul class="nav navbar-nav ">
 			<!-- Menu principal -->
 			<!-- /.navbar-collapse -->
@@ -105,7 +107,7 @@
 			<?php include ('Login.php');?>
 
 
-		</div>
+		</div>  <!-- /.navbar-collapse -->
 		<!-- /.container -->
 	</nav>
 
